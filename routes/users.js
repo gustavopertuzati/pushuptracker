10c65d7a752ea389c51b7de949ae08fbbb4ddc10
gustavo.pertuzat <gustavo.pertuzati@hesge.ch>
2023-02-24 16:13:00 +0100

    date

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+// middleware that is specific to this router and connects to the database
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
